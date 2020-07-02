4f00f802dcec098acd662d31d460a414dc7c4f1e
Caroline <carolinejepps@gmail.com>
2020-07-02 23:35:30 +0000

    Added refactored index and breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
 const request = require('request');
 
 // take in query from CM
-const breed = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv.slice(2);
 
-console.log(breed);
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response) => {
+    if (error) {
+      callback(error);
+      return; // Print the error if one occurred
+    }
+    
+    const data = JSON.parse(response.body);
+   
+    if (data.length > 0) {
+      callback(null, data[0].description); // Print the description
+    } else {
+      callback(null, 'Breed not found');
+    }
+  });
+};
 
 
-
-request(breed, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    console.log('description:', data[0].description); // Print the description
-  } else {
-    console.log('Breed not found');
-  }
-});
+module.exports = { fetchBreedDescription };
